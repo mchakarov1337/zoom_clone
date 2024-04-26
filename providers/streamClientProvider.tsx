519c55f318b4c718f6587ac90aff4c65b1c1b617
martinChakarov1337 <martin.chakarov121@gmail.com>
2024-04-26 22:44:00 +0300

    homePage components added

---
+++ providers/streamClientProvider.tsx
@@ -0,0 +1,10 @@
+import { StreamVideo, StreamVideoClient } from '@stream-io/video-react-sdk';
+import { ReactNode } from 'react';
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  return <StreamVideo client={client}></StreamVideo>;
+};
+
+export default StreamVideoProvider;
