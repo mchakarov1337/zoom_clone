df2f4af9a6c02a0c6fcfcf36f1c5b8768155cee1
martinChakarov1337 <martin.chakarov121@gmail.com>
2024-04-27 12:38:51 +0300

    created customHook for returning current call

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,7 +4,8 @@ import HomeCard from './HomeCard';
 import { useRouter } from 'next/navigation';
 import MeetingModal from './MeetingModal';
 import { useUser } from '@clerk/nextjs';
-import { useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useToast } from '@/components/ui/use-toast';
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -13,16 +14,54 @@ const MeetingTypeList = () => {
   >();
   const user = useUser();
   const client = useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: '',
+    link: '',
+  });
+  const [callDetails, setCallDetails] = useState<Call>();
+  const { toast } = useToast();
 
-  const createMeeting = () => {
+  const createMeeting = async () => {
     if (!user || !client) {
       return;
     }
 
     try {
+      if (!values.dateTime) {
+        toast({ title: 'Please select a date and a time.' });
+        return;
+      }
+
       const id = crypto.randomUUID();
+      const call = client.call('default', id);
+
+      if (!call) {
+        throw new Error('Failed to create call!');
+      }
+
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || 'Instant Meeting';
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+
+      setCallDetails(call);
+
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({ title: 'Meeting Created.' });
     } catch (error) {
       console.log(error);
+      toast({ title: 'Failed to create Meeting' });
     }
   };
 
